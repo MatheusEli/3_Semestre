611fc9410ca9077ea8b12caf7d05fdd4ad3abb47
Matheus Eli F <matheuselisouzaferreira@hotmail.com>
2018-03-17 17:13:37 -0300

    Mudancas

--- Estrutura_de_Dados/Aula_1/src/Exercicios/Exercicio_2.java
+++ Estrutura_de_Dados/Aula_1/src/Exercicios/Exercicio_2.java
@@ -1,11 +1,74 @@
 package Exercicios;
+
+import java.util.Scanner;
+
 public class Exercicio_2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.in);
+        Scanner ler = new Scanner(System.in);
+        
+        int qtdpessoas = 0,qtdpessoas90 = 0,qtdpessoas190=0,qtdpessoas10e30=0, acmidades = 0;
+         
+        
+        Pessoa_EX2 pessoas[] = new Pessoa_EX2[2];
+        
+        
+        for(int i=0; i<pessoas.length; i++){
+            
+            System.out.println("Digite o nome da pessoa: ");
+            String nome = entrada.nextLine();
+            
+            System.out.println("Digite a idade de "+nome+": ");
+            int idade = ler.nextInt();
+            
+            acmidades = acmidades+idade;
+            
+            System.out.println("Digite o peso de "+nome+": ");
+            double peso = ler.nextDouble();
+            
+            System.out.println("Digite a altura de "+nome+": ");
+            double altura = ler.nextDouble();
+            
+            
+            if(peso > 90  &&  altura < 1.50){
+                
+                qtdpessoas90++;
+            }
+            
+            if(altura > 1.90){
+                
+                if(idade >= 10  && idade <= 30){
+                    
+                    qtdpessoas10e30 ++;
+                }
+                
+                qtdpessoas190 ++;
+            }
+            
+            pessoas[i] = new Pessoa_EX2(idade,peso,altura);
+            
+            qtdpessoas ++;
+        }
+        
+        
+        
+        System.out.println("Média das Idades: "+(acmidades/qtdpessoas));
+        System.out.println("Quantidade de pessoas com peso superior a 90 e altura inferior a 1.50: "+qtdpessoas90);
+        
+        try{
+            int porcentagem = ((qtdpessoas10e30*100)/qtdpessoas190);
+            
+            System.out.println("Porcentagem de pessoas com idade entre 10 e 30 anos entre as pessoas com mais de 1,90 de altura: "+porcentagem+"%");
+        }catch(ArithmeticException ex){
+            System.out.println("Não há pessoas com mais de 1,90m!");
+        }
+        
+        
+        
+        
+        
     }
     
 }
---
+++ Estrutura_de_Dados/Aula_1/src/Exercicios/Pessoa_EX2.java
@@ -0,0 +1,40 @@
+package Exercicios;
+
+public class Pessoa_EX2 {
+    
+    private int idade;
+    private double peso;
+    private double altura;
+
+    public Pessoa_EX2(int idade, double peso, double altura) {
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    
+}
